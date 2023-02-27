313fcbd8da657f4ca2e013f604926f017c2e72df
alexissoko <alesoko@live.com>
2023-02-27 14:20:00 -0300

    translation of models attributes implemented

--- charts/models.py
+++ charts/models.py
@@ -26,8 +26,8 @@ SAFEBOX_CHOICES = (
 # Create your models here.
 class Owner(models.Model):
     name = models.CharField(max_length=100, help_text=_('First and last name.'), verbose_name=_('name'))
-    date = models.DateField(null=True, blank=True, auto_now_add=True)
-    description = models.TextField(null=True, blank=True)
+    date = models.DateField(null=True, blank=True, auto_now_add=True, verbose_name=_('date'))
+    description = models.TextField(null=True, blank=True, verbose_name=_('description'))
     # total = models.IntegerField(blank=True)
 
     def __str__(self) -> str:
@@ -36,9 +36,9 @@ class Owner(models.Model):
 
 # Create your models here.
 class Sport(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateField(null=True, blank=True, auto_now_add=True)
-    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=100, verbose_name=_('name'))
+    date = models.DateField(null=True, blank=True, auto_now_add=True, verbose_name=_('date'))
+    description = models.TextField(null=True, blank=True, verbose_name=_('description'))
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE, blank=True, null=True)
     def __str__(self) -> str:
         return self.name
@@ -46,9 +46,12 @@ class Sport(models.Model):
 
 # Create your models here.
 class Activity(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateField(default=timezone.now)
-    description = models.TextField(null=True, blank=True)
+    class Meta:
+        verbose_name = _('acitivity')
+        verbose_name_plural = _('activities')
+    name = models.CharField(max_length=100, verbose_name=_('name'))
+    date = models.DateField(default=timezone.now, verbose_name=_('date'))
+    description = models.TextField(null=True, blank=True, verbose_name=_('description'))
     quota = models.IntegerField(blank=True)
     sport = models.ForeignKey(Sport, on_delete=models.CASCADE, blank=True, null=True)
 
@@ -59,26 +62,30 @@ class Activity(models.Model):
 
 # Create your models here.
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateField(null=True, blank=True, auto_now_add=True)
-    description = models.TextField()
+    class Meta:
+        verbose_name = _('customer')
+        verbose_name_plural = _('customers')
+        
+    name = models.CharField(max_length=100, verbose_name=_('name'))
+    description = models.TextField(verbose_name=_('description'))
+    date = models.DateField(null=True, blank=True, auto_now_add=True, verbose_name=_('date'))
     
     def __str__(self) -> str:
-        return self.name
+        return  self.name
 
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateField(null=True, blank=True, auto_now_add=True)
+    name = models.CharField(max_length=100, verbose_name=_('name'))
+    date = models.DateField(null=True, blank=True, auto_now_add=True, verbose_name=_('date'))
     customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE, blank=True, null=True
     )
     activity = models.ForeignKey(
         Activity, on_delete=models.CASCADE, blank=True, null=True
     )
-    description = models.TextField()
-    status = models.BooleanField(verbose_name='Active', default=True)
+    description = models.TextField(verbose_name=_('description'))
+    status = models.BooleanField(verbose_name=_('active'), default=True)
 
     def __str__(self) -> str:
         return self.name
@@ -88,11 +95,11 @@ class Membership(models.Model):
     # invoice=models.ForeignKey(Input, on_delete=models.CASCADE)
     # seller=models.ForeignKey(Provider, on_delete=models.CASCADE, blank=True, null=True)
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE, blank=True, null=True)
-    date = models.DateField(default=timezone.now)
+    date = models.DateField(default=timezone.now, verbose_name=_('date'))
     fee = models.BooleanField(verbose_name='Inscription')
     price = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True, verbose_name=_('description'))
    
 
     def __str__(self) -> str:
@@ -109,7 +116,7 @@ class TypePayment(models.Model):
     membership = models.ForeignKey(
         Membership, on_delete=models.CASCADE, blank=True, null=True
     )
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True, verbose_name=_('description'))
     membership.quantity = 1
 
     def __str__(self) -> str:
@@ -118,12 +125,12 @@ class TypePayment(models.Model):
 
 class Payment(models.Model):
     # invoice=models.ForeignKey(Input, on_delete=models.CASCADE)
-    date = models.DateField(default=timezone.now)
+    date = models.DateField(default=timezone.now, verbose_name=_('date'))
     time = models.TimeField(default=datetime.now().strftime("%H:%M:%S"))
     payment_type = models.ForeignKey(TypePayment, on_delete=models.CASCADE, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     receiver = models.ForeignKey(Owner, on_delete=models.CASCADE, blank=True, null=True)
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True, verbose_name=_('description'))
     price = models.IntegerField()
     # TODO: 12 quotas generator funcitonality
     quota_number = models.TextField(null=True, blank=True)#choices=MONTHLY_QUOTAS)
