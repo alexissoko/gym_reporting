9e8281e31a2371c047ca83fb78cf0e7a30fd70cc
alexissoko <alesoko@live.com>
2023-03-17 14:09:08 -0300

    final demo dev complete

--- charts/urls.py
+++ charts/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('reporting/sales', views.reporting_sales, name='reporting_sales'),
     path('reporting/payments', views.reporting_payments, name='reporting_payments'),
+    path('reporting/<int:pk>/payments', views.reporting_invoice, name='reporting_invoice'),
     path('reporting/activities', views.reporting_activities, name='reporting_activities'),
+    path('reporting/expenses', views.reporting_expenses, name='outbound_reporting'),
+    path('payment/search/', views.payment_search, name='payment_search'),
+    path('post/new/', views.post_new, name='post_new'),
+    # path('post/<int:pk>/edit/', views.post_detail, name='post_detail'),
 ]  # end urlpatterns
